cf5884bf1de38cec966e7653e1f647f61aef2198
egalawan <thisuranawalage@gmail.com>
2023-12-19 23:21:35 -0800

    Have Minified JSON info on localhost:3000/ but nothing else. Stuck

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const db = require('./database'); // Assuming this is your database configuration file
 
+
+// Define a route handler for the root path ("/")
+app.get('/', (req, res) => {
+    // res.send('Welcome to the Business Listings App');
+    res.redirect('/test-db');
+});
+
 // Define your routes after initializing 'app'
 app.get('/test-db', (req, res) => {
     db.query('SELECT * FROM business', (err, results) => {
@@ -12,22 +19,27 @@ app.get('/test-db', (req, res) => {
     });
 });
 
-app.get('/businesses', (req, res) => {
-    let page = parseInt(req.query.page) || 1;
-    let limit = parseInt(req.query.limit) || 25;
-    let offset = (page - 1) * limit;
-
-    const query = 'SELECT name, type, location, rating FROM business LIMIT ?, ?';
-    db.query(query, [offset, limit], (err, results) => {
-        if (err) {
-            console.error('Error executing query:', err);
-            return res.status(500).send('Error on the server.');
-        }
-        res.json(results);
+app.get('/filter', (req, res) => {
+    const { name, type, location, page, pageSize } = req.query;
+    const offset = (page - 1) * pageSize; // Calculate the offset
+  
+    const query = `
+      SELECT name, type, location, rating
+      FROM business
+      WHERE name LIKE ? AND type LIKE ? AND location LIKE ?
+      LIMIT ?, ?;`;
+    const values = [`%${name}%`, `%${type}%`, `%${location}%`, offset, parseInt(pageSize)];
+  
+    db.query(query, values, (err, results) => {
+      if (err) {
+        console.error('Error querying the database:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      res.json(results);
     });
-});
-
-
+  });
+  
+  
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-let currentPage = 1;
-const loadData = () => {
-    fetch(`http://localhost:3000/businesses?page=${currentPage}`)
-        .then(response => response.json())
-        .then(data => {
-            const container = document.getElementById('data-container');
-            data.forEach(business => {
-                const div = document.createElement('div');
-                div.textContent = `Name: ${business.name}, Type: ${business.type}, Location: ${business.location}, Rating: ${business.rating}`;
-                container.appendChild(div);
-            });
-            currentPage++;
-        })
-        .catch(error => console.error('Error:', error));
-};
+// script.js
 
-loadData(); // Load initial data
+// Function to fetch and display data based on filters and pagination
+function applyFilters() {
+    const name = document.getElementById('nameFilter').value;
+    const type = document.getElementById('typeFilter').value;
+    const location = document.getElementById('locationFilter').value;
+    const page = 1; // Set your initial page number
+    const pageSize = 25; // Set the number of items per page
+  
+    fetch(`/filter?name=${name}&type=${type}&location=${location}&page=${page}&pageSize=${pageSize}`)
+      .then(response => response.json())
+      .then(data => {
+        // Display the data in the data-container element (modify this according to your HTML structure)
+        const dataContainer = document.getElementById('data-container');
+        dataContainer.innerHTML = ''; // Clear previous data
+  
+        data.forEach(item => {
+          const listItem = document.createElement('div');
+          listItem.textContent = `Name: ${item.name}, Type: ${item.type}, Location: ${item.location}, Rating: ${item.rating}`;
+          dataContainer.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  
+  // Function to load more data (pagination)
+  function loadMoreData() {
+    const name = document.getElementById('nameFilter').value;
+    const type = document.getElementById('typeFilter').value;
+    const location = document.getElementById('locationFilter').value;
+    const currentPage = 2; // Change this according to your needs
+    const pageSize = 25; // Set the number of items per page
+  
+    fetch(`/filter?name=${name}&type=${type}&location=${location}&page=${currentPage}&pageSize=${pageSize}`)
+      .then(response => response.json())
+      .then(data => {
+        // Display the data (append to existing data)
+        const dataContainer = document.getElementById('data-container');
+  
+        data.forEach(item => {
+          const listItem = document.createElement('div');
+          listItem.textContent = `Name: ${item.name}, Type: ${item.type}, Location: ${item.location}, Rating: ${item.rating}`;
+          dataContainer.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  
+  // Add event listeners to the filter and load more buttons
+  document.getElementById('applyFiltersBtn').addEventListener('click', applyFilters);
+  document.getElementById('loadMoreBtn').addEventListener('click', loadMoreData);
+  
